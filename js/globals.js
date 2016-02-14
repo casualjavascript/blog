3db989d56cec8a2fb626b4a3f7be9c35aa64066f
mateogianolio <gianoliomateo@gmail.com>
2016-02-14 02:34:30 +0100

    upd globals

--- js/globals.js
+++ js/globals.js
@@ -1,5 +1,5 @@
 'use strict';
 
-var USERNAME = 'mateogianolio', // github username
+var USERNAME = 'casualjavscript', // github username
     REPO = 'openblog', // github repository
-    TOKEN = '065d9dada1a528b2a741cc5a9699f1be2589644c'; // github app access token
+    TOKEN = '549759eee756900a1c491bebb0062c42bbc442fd'; // github app access token
