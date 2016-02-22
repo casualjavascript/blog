fc3b403fc30d5c7d444582b82dd2523cdad69219
mateogianolio <gianoliomateo@gmail.com>
2016-02-22 14:40:49 +0100

    improved article rendering time

--- js/render.js
+++ js/render.js
@@ -12,13 +12,13 @@ function order(a, b) {
     0 : -1;
 }
 
-// generates html from a github issue
-function generate(issue) {
-  var search = window.location.search;
-  if (search && search.indexOf(issue.id) === -1)
+// renders issue as article
+function post(error, issue) {
+  if (error || !issue) {
+    // redirect to front page if id incorrent
+    document.location.replace('index.html');
     return;
-  else if (search)
-    document.title = issue.title;
+  }
 
   var parent = document.querySelector('.threads'),
       content = [];
@@ -33,7 +33,7 @@ function generate(issue) {
   content.push((issue.comments === 1 ? '1 comment' : (issue.comments ? (issue.comments + ' comment(s)') : 'no comments')));
   content.push('</div>');
 
-  if (search && issue.labels.length) {
+  if (issue.labels.length) {
     content.push('<div class="post-meta-categories">');
     issue.labels.forEach(function (label) {
       content.push('<span class="post-meta-category" style="background: #' + label.color + '">');
@@ -43,18 +43,14 @@ function generate(issue) {
     content.push('</div>');
   }
 
-  if (search) {
-    content.push('<div class="post-body">');
-    content.push(marked(issue.body).replace(/<pre>/g, '<pre class="prettyprint">'));
-    content.push('</div>');
-  }
-
+  content.push('<div class="post-body">');
+  content.push(marked(issue.body).replace(/<pre>/g, '<pre class="prettyprint">'));
   content.push('</div>');
 
   parent.innerHTML += content.join('');
   prettyPrint();
 
-  if (issue.comments && search) {
+  if (issue.comments) {
     var commentContainer = document.createElement('div');
     commentContainer.className = 'post-comments';
     commentContainer.innerHTML = 'Loading ' + issue.comments + ' comments...';
@@ -86,19 +82,45 @@ function generate(issue) {
 
       prettyPrint();
     });
-  } else if (search)
+  } else {
     parent.innerHTML += '<a href="' + issue.html_url + '">add comment</a>';
+  }
+}
+
+// lists all issues (front page)
+function list(error, issues) {
+  if (error || !issues)
+    return;
+
+  issues = issues.sort(order);
+  var parent = document.querySelector('.threads'),
+      content = [];
+
+  issues.forEach(function (issue) {
+    content.push('<div id="' + issue.id + '" class="post">');
+    content.push('<h1 class="post-title">');
+    content.push('<a href="?' + issue.number + '">' + issue.title + '</a>');
+    content.push('</h1>');
+    content.push('<div class="post-meta">');
+    content.push('by <a href="' + issue.user.html_url + '">' + issue.user.login + '</a> &middot; ');
+    content.push(new Date(issue.created_at).toLocaleDateString() + ' &middot; ');
+    content.push((issue.comments === 1 ? '1 comment' : (issue.comments ? (issue.comments + ' comment(s)') : 'no comments')));
+    content.push('</div>');
+    content.push('</div>');
+  });
+
+  parent.innerHTML += content.join('');
 }
 
 // renders github issues
 function render() {
-  github
-    .getIssues(USERNAME, REPO)
-    .list({}, function (error, issues) {
-      if (error || !issues)
-        return;
+  document.querySelector('.threads').innerHTML = '';
 
-      document.querySelector('.threads').innerHTML = '';
-      issues.sort(order).forEach(generate);
-    });
+  var issues = github.getIssues(USERNAME, REPO),
+      id = window.location.search.replace('?', '');
+
+  if (id) {
+    issues.get(id, post);
+  } else
+    issues.list({}, list);
 }
